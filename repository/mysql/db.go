64b448336739e7b3aa1a156fdf968b74a04ae513
Ali-Noori <alinoori021110@gmail.com>
2026-01-03 17:00:50 +0330

    feat(app): implement auth service with access_token and refresh_token and add validator core logic and richerror

--- repository/mysql/db.go
+++ repository/mysql/db.go
@@ -2,23 +2,39 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type MySQLDB struct{
-	db *sql.DB
+type Config struct {
+	Username string
+	Password string
+	Port     int
+	Host     string
+	DBName   string
 }
 
-func New() *MySQLDB {
+type MySQLDB struct {
+	config Config
+	db     *sql.DB
+}
 
-	db, err := sql.Open("mysql", "user:password@/dbname")
+func New(config Config) *MySQLDB {
+	// parseTime=true changes the output type of DATE and DATETIME values to time.Time
+	// instead of []byte / string
+	// The date or datetime like 0000-00-00 00:00:00 is converted into zero value of time.Time
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s:%d)/%s?parseTime=true",
+		config.Username, config.Password, config.Host, config.Port, config.DBName))
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("can't open mysql db: %v", err))
 	}
+
+	// See "Important settings" section.
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
-	return &MySQLDB{db}
+
+	return &MySQLDB{config: config, db: db}
 }
--- repository/mysql/user.go
+++ repository/mysql/user.go
@@ -1,9 +1,87 @@
 package mysql
 
-func (d DB) IsPhoneNumberUnique(phonenumber string) (bool, error) {
+import (
+	"database/sql"
+	"fmt"
+	"gameapp/entity"
+	"gameapp/pkg/errmsg"
+	"gameapp/pkg/richerror"
+	"time"
+)
 
+func (d *MySQLDB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
+	const op = "mysql.IsPhoneNumberUnique"
+
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
+
+	_, err := scanUser(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true, nil
+		}
+
+		return false, richerror.New(op).WithErr(err).
+			WithMessage(errmsg.ErrorMsgCantScanQueryResult).WithKind(richerror.KindUnexpected)
+	}
+
+	return false, nil
 }
 
-func (d DB) Register(phonenumber string) (bool, error) {
-	
+func (d *MySQLDB) Register(u entity.User) (entity.User, error) {
+	res, err := d.db.Exec(`insert into users(name, phone_number, password) values(?, ?, ?)`, u.Name, u.PhoneNumber, u.Password)
+	if err != nil {
+		return entity.User{}, fmt.Errorf("can't execute command: %w", err)
+	}
+
+	// error is always nil
+	id, _ := res.LastInsertId()
+	u.ID = uint(id)
+
+	return u, nil
+}
+
+func (d *MySQLDB) GetUserByPhoneNumber(phoneNumber string) (entity.User, bool, error) {
+	const op = "mysql.GetUserByPhoneNumber"
+
+	row := d.db.QueryRow(`select * from users where phone_number = ?`, phoneNumber)
+
+	user, err := scanUser(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return entity.User{}, false, nil
+		}
+
+		return entity.User{}, false, richerror.New(op).WithErr(err).
+			WithMessage(errmsg.ErrorMsgCantScanQueryResult).WithKind(richerror.KindUnexpected)
+	}
+
+	return user, true, nil
+}
+
+func (d *MySQLDB) GetUserByID(userID uint) (entity.User, error) {
+	const op = "mysql.GetUserByID"
+
+	row := d.db.QueryRow(`select * from users where id = ?`, userID)
+
+	user, err := scanUser(row)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return entity.User{}, richerror.New(op).WithErr(err).
+				WithMessage(errmsg.ErrorMsgNotFound).WithKind(richerror.KindNotFound)
+		}
+
+		return entity.User{}, richerror.New(op).WithErr(err).
+			WithMessage(errmsg.ErrorMsgCantScanQueryResult).WithKind(richerror.KindUnexpected)
+	}
+
+	return user, nil
+}
+
+func scanUser(row *sql.Row) (entity.User, error) {
+	var createdAt time.Time
+	var user entity.User
+
+	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &createdAt, &user.Password)
+	fmt.Println("createdAt", createdAt)
+	return user, err
 }
