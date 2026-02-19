e639c6ba1d24d6f935880bb532414b165e49067d
Ali-Noori <a@as-MacBook-Pro.local>
2026-02-19 14:14:44 +0330

    feat(matching-engine): add match waited users logic for each category using concurrent programming

--- service/matchingservice/service.go
+++ service/matchingservice/service.go
@@ -1,15 +1,26 @@
 package matchingservice
 
 import (
+	"context"
 	"fmt"
 	"gameapp/entity"
 	"gameapp/param"
 	"gameapp/pkg/richerror"
+	"gameapp/pkg/timestamp"
+	"sync"
 	"time"
+
+	funk "github.com/thoas/go-funk"
 )
 
+// TODO - add ctx to ~all repo and use-case methods
 type Repo interface {
 	AddToWaitingList(userID uint, category entity.Category) error
+	GetWaitingListByCategory(ctx context.Context, category entity.Category) ([]entity.WaitingMember, error)
+}
+
+type PresenceClient interface {
+	GetPresence(ctx context.Context, request param.GetPresenceRequest) (param.GetPresenceResponse, error)
 }
 
 type Config struct {
@@ -17,12 +28,13 @@ type Config struct {
 }
 
 type Service struct {
-	config Config
-	repo   Repo
+	config         Config
+	repo           Repo
+	presenceClient PresenceClient
 }
 
-func New(config Config, repo Repo) Service {
-	return Service{config: config, repo: repo}
+func New(config Config, repo Repo, presenceClient PresenceClient) Service {
+	return Service{config: config, repo: repo, presenceClient: presenceClient}
 }
 
 func (s Service) AddToWaitingList(req param.AddToWaitingListRequest) (
@@ -39,7 +51,73 @@ func (s Service) AddToWaitingList(req param.AddToWaitingListRequest) (
 	return param.AddToWaitingListResponse{Timeout: s.config.WaitingTimeout}, nil
 }
 
-func (s Service) MatchWaitedUsers(req param.MatchWaitedUsersRequest) (param.MatchWaitedUsersResponse, error) {
-	fmt.Println("MatchWaitedUsers", time.Now())
+func (s Service) MatchWaitedUsers(ctx context.Context, _ param.MatchWaitedUsersRequest) (param.MatchWaitedUsersResponse, error) {
+	const op = richerror.Op("matchingservice.MatchWaitedUsers")
+
+	var wg sync.WaitGroup
+	for _, category := range entity.CategoryList() {
+		wg.Add(1)
+		go s.match(ctx, category, &wg)
+	}
+
+	wg.Wait()
+
 	return param.MatchWaitedUsersResponse{}, nil
+}
+
+func (s Service) match(ctx context.Context, category entity.Category, wg *sync.WaitGroup) {
+	const op = richerror.Op("matchingservice.match")
+
+	defer wg.Done()
+
+	list, err := s.repo.GetWaitingListByCategory(ctx, category)
+	if err != nil {
+		// TODO - log error
+		// TODO - update metrics
+		return
+	}
+
+	userIDs := make([]uint, len(list))
+	for _, l := range list {
+		userIDs = append(userIDs, l.UserID)
+	}
+
+	presenceList, err := s.presenceClient.GetPresence(ctx, param.GetPresenceRequest{UserIDs: userIDs})
+	if err != nil {
+		// TODO - log error
+		// TODO - update metrics
+		return
+	}
+
+	presenceUserIDs := make([]uint, len(list))
+	for _, l := range presenceList.Items {
+		presenceUserIDs = append(presenceUserIDs, l.UserID)
+	}
+
+	// TODO - merge presenceList with list based on userID
+	// also consider the presence timestamp of each user
+	// and remove users from waiting list if the user's timestamp is older than time.Now(-20 seconds)
+	//if t < timestamp.Add(-20*time.Second) {
+	//	// remove from list
+	//}
+
+	var finalList = make([]entity.WaitingMember, 0)
+	for _, l := range list {
+		if funk.ContainsUInt(presenceUserIDs, l.UserID) && l.Timestamp < timestamp.Add(-20*time.Second) {
+			finalList = append(finalList, l)
+		} else {
+			// remove from waiting list
+		}
+	}
+
+	for i := 0; i < len(finalList)-1; i = i + 2 {
+
+		mu := entity.MatchedUsers{
+			Category: category,
+			UserID:   []uint{finalList[i].UserID, finalList[i+1].UserID},
+		}
+		fmt.Println("mu", mu)
+		// publish a new event for mu
+		// remove mu users from waiting list
+	}
 }
